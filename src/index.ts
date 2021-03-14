8facbff79ec5249099c77de04be5659ded76d5af
fumi uk <dcfumiya.238@gmail.com>
2021-03-15 08:35:28 +0900

    ts-basic

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,23 @@
 // world.sayHello(root);
 
 // 03.基本の型定義
-import { anySample, primitiveSample, notExistSample, unknownSample } from "./basic";
+// import { anySample, primitiveSample, notExistSample, unknownSample } from "./basic";
 
-primitiveSample();
-notExistSample();
-anySample();
-unknownSample();
+// primitiveSample();
+// notExistSample();
+// anySample();
+// unknownSample();
+
+// 04.関数の型定義
+// import { alwaysThrowError, logMessage, logMessage2, logMessage3, logMessage4 } from "./function/basic";
+import { isUserSignedIn, isUserSignedIn2 } from "./function/parameters";
+
+// logMessage("Hello TypeScript!");
+// logMessage2("Hello TypeScript!");
+// logMessage3("Hello TypeScript!");
+// logMessage4("Hello TypeScript!");
+// alwaysThrowError("Hello TypeScript!");
+
+isUserSignedIn("ABC", "user");
+isUserSignedIn("DEF");
+isUserSignedIn2("ABC");
